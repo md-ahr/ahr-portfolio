97ff23ce3f9e97d1ac5983ba1f90a0c477fee97a
Abdul Halim <halim@rokomari.com>
2022-11-30 20:38:49 +0600

    Design nav section and header title section and refactoring css code

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,18 @@
 const navbar = () => {
   return (
-    <div>navbar</div>
+    <nav className="navbar navbar-expand">
+      <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a aria-current="page" className="nav-link active" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Work</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Contact</a>
+        </li>
+      </ul>
+    </nav>
   );
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,18 @@ import Navbar from '../components/Navbar';
 
 const Home = () => {
   return (
-    <Navbar />
+    <div className="custom-container">
+      <Navbar />
+      <p className='top-content'>
+        <span>Turn your</span>
+        <span>Out of the box</span>
+        <span>ideas into</span>
+        <span>Reality</span>
+      <label className='line'></label>
+      <label className='line2'></label>
+      <label className='line3'></label>
+      </p>
+    </div>
   );
 };
 
